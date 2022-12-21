45961cbcd152ecfa3f0ed601c6eb3fe2de0a806c
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-12-21 18:26:56 +0500

    add visits project for docker conmpose

---
+++ visits/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const redis= require("redis")
+const port = 3000
+
+const clientDB= redis.createClient()
+clientDB.set('visits',10)
+
+
+
+
+app.get('/', (req, res) =>{
+    clientDB.get("visits",(err,visits)=>{
+        res.send("Visits number is ",visits)
+        clientDB.set('visits',visits+1)
+    })
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
