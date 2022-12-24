b76c3acc0b5eccc7a93ac58e2ed4a65435c108cb
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-12-24 13:47:36 +0500

    docker projects

---
+++ express-docker/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('Hello world');
+});
+const redisClient = require('./redis-client');
+app.get('/store/:key', async (req, res) => {
+    const { key } = req.params;
+    const value = req.query;
+    await redisClient.setAsync(key, JSON.stringify(value));
+    return res.send('Success');
+});
+app.get('/:key', async (req, res) => {
+    const { key } = req.params;
+    const rawData = await redisClient.getAsync(key);
+    return res.json(JSON.parse(rawData));
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
